cd92f15321984bf92538410292faa0ce9674ce90
Kyle Semelka <kyle.semelka@figure.ai>
2024-03-20 07:26:21 +0000

    Cross-platform examples working

--- WORKSPACE
+++
@@ -1,7 +0,0 @@
-load("@rules_poetry//python:poetry.bzl", "poetry_update")
-
-poetry_update(
-    name = "update_lock",
-    toml = "pyproject.toml",
-    lock = "poetry.lock",
-)
---
+++ examples/numpy/app.py
@@ -0,0 +1,6 @@
+import sys
+import numpy as np
+
+if __name__ == "__main__":
+    # Execute example numpy code just to make sure library is loaded
+    print(np.arange(15).reshape(3, 5))
